5d2d918f5f4041586972236f6268ba5ec4789df8
Ishara Dilshan <isharadilshandev@gmail.com>
2022-05-24 20:33:39 +0530

    setup component structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import type {Node} from 'react';
+import HomeScreen from './screens/HomeScreen';
+
+const App: () => Node = () => {
+  return <HomeScreen />;
+};
+
+export default App;
